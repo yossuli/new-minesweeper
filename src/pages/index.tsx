aa60ee1be038b34ab000489cd3bccf831e30fe25
yossuli <s1f102302348@iniad.org>
2024-06-21 20:09:33 +0900

    基本の見た目

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
 import styles from './index.module.css';
 
 const Home = () => {
-  return <div className={styles.container}>{}</div>;
+  const board = [
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+  ];
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <div className={styles.head} />
+        <div className={styles.board}>
+          {board.map((row, y) =>
+            row.map((cell, x) => (
+              <div className={cell !== -1 ? styles.cell : styles.stone} key={`${x}-${y}`} />
+            )),
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
