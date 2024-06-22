80a25aea4d28898686acb9b0259f81f4f406aa31
yossuli <s1f102302348@iniad.org>
2024-06-22 10:50:08 +0900

    クリックしたらそのマスの周りのボムの数が出る

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
 import { useState } from 'react';
 import styles from './index.module.css';
+const dirs = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+];
 
 const Home = () => {
   const board = [
@@ -25,15 +35,15 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const [bombMap, setBombMap] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0],
   ]);
   const clickHandler = (x: number, y: number) => {
     const newUserInputs = structuredClone(userInputs);
@@ -42,15 +52,27 @@ const Home = () => {
     console.table(newUserInputs);
     setUserInputs(newUserInputs);
   };
+  const countBomb = (x: number, y: number) => {
+    let bombCount = 0;
+    for (const [dx, dy] of dirs) {
+      if (bombMap[y + dy] !== undefined && bombMap[y + dy][x + dx] === 1) {
+        console.log(y + dy, x + dx);
+        bombCount++;
+      }
+    }
+    return bombCount;
+  };
   for (let y = 0; y < 9; y++) {
     for (let x = 0; x < 9; x++) {
       if (userInputs[y][x] === 1) {
-        board[y][x] = 0;
+        board[y][x] = countBomb(x, y);
       }
     }
   }
   console.log('userInputs');
   console.table(userInputs);
+  console.log('board');
+  console.table(board);
   return (
     <div className={styles.container}>
       <div className={styles.main}>
@@ -61,6 +83,7 @@ const Home = () => {
               <div
                 className={cell !== -1 ? styles.cell : styles.stone}
                 onClick={() => clickHandler(x, y)}
+                style={{ backgroundPositionX: `${(cell - 1) * -30}px` }}
                 key={`${x}-${y}`}
               />
             )),
